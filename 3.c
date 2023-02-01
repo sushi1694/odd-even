33607b05ec9239120c90dfbebc0127f75270c437
sushi1694 <124113986+sushi1694@users.noreply.github.com>
2023-01-31 23:01:46 -0800

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the no: ");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("The no is even");
+	}
+	else
+	{
+		printf("The given no is odd");
+	}
+	return 0;
+}
